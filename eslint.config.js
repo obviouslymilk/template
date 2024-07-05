b504598afeb9f351302663be8829e7f4a43410b3
Klim Storozhenko <13135397-obviouslymilk@users.noreply.gitlab.com>
2024-07-05 10:41:43 +0300

    Install ESLint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.strict,
+    ...tseslint.configs.stylistic,
+    ...pluginVue.configs['flat/recommended'],
+    {
+        files: ['*.vue', '**/*.vue', '*.ts', '**/*.ts'],
+        languageOptions: {
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                sourceType: 'module',
+            }
+        }
+    }
+)
