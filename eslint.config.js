c0425be0f2a8adfc391842cfb1f6f6793a38620e
Klim Storozhenko <13135397-obviouslymilk@users.noreply.gitlab.com>
2024-07-08 13:02:32 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,21 @@ export default ts.config(
       '@stylistic/quotes': ['error', 'single'],
 
       'no-var': 'error',
+      'no-new-object': 'error',
+      'object-shorthand': 'error',
+      'quote-props': ['error', 'as-needed'],
       'no-array-constructor': 'error',
+      'prefer-destructuring': ['error', {
+        array: true,
+        object: true
+      },
+        {
+          enforceForRenamedProperties: true
+        }],
+      'func-style': ['error', 'expression'],
+      'func-names': ['error', 'as-needed'],
+      'no-loop-func': 'error',
+      'prefer-rest-params': 'error'
     },
   },
 );
