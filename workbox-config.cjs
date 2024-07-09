8c5326594127a8223ed1e9b00992dc8e7c1b792e
Klim Storozhenko <13135397-obviouslymilk@users.noreply.gitlab.com>
2024-07-09 10:04:38 +0300

    install workbox

---
+++ workbox-config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  globDirectory: 'dist/',
+  globPatterns: [
+    '**/*.{js,css,woff2}',
+  ],
+  swDest: 'dist/sw.js',
+  ignoreURLParametersMatching: [
+    /^utm_/,
+    /^fbclid$/,
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|webp|ico)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+      },
+    },
+  ],
+};
