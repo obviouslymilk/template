0054fb7cfb9ad3d4f85ab37be11a8487227b32e0
Klim Storozhenko <13135397-obviouslymilk@users.noreply.gitlab.com>
2024-08-09 11:59:41 +0300

    make Inter 4.0 a default font for a template.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { type Component, createApp } from 'vue';
 import '@/assets/reset.css';
+import '@/assets/index.css';
 import App from './App.vue';
 
 const app = createApp(App as Component);
