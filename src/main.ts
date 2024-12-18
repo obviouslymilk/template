4db46a4a1cf7c994a4d4851ae0f9b327ea301b25
Klim <storozhenko.klim@gmail.com>
2024-12-18 16:21:17 +0300

    refactor: rename app component to lowercase

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import '@/assets/styles/index.css';
 import '@/assets/styles/reset.css';
 import { type Component, createApp } from 'vue';
-import App from './App.vue';
+import App from './app.vue';
 
 const app = createApp(App as Component);
 app.mount('#app');
