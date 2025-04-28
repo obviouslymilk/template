53379e3f88e7c47952bd8496035340301b7146e8
Klim <storozhenko.klim@gmail.com>
2025-04-28 14:35:33 +0300

    refactor(router): change index-page path

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
+import IndexPage from '@/pages/index-page.vue';
 import { Component } from 'vue';
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import IndexPage from './components/pages/index-page.vue';
 
 const routes = [
   {
