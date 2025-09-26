694a19b510dbb88cbecbaf5cd07aa2ef62250a4a
Klim <storozhenko.klim@gmail.com>
2025-09-26 12:05:03 +0300

    config(postcss): add preset-env plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,10 @@
-import autoprefixer from 'autoprefixer';
-import postcssNesting from 'postcss-nesting';
 import postcssPresetEnv from 'postcss-preset-env';
 
 /** @type {import('postcss-load-config').Config} */
 export default {
   plugins: [
-    autoprefixer,
-    postcssNesting,
-    postcssPresetEnv,
+    postcssPresetEnv({
+      stage: 2
+    }),
   ],
 };
